6b9f74107418d9bd9c1d1254f79aadf9f6582b3c
LongNghia <longnghia2.00@gmail.com>
2023-03-25 20:42:16 +0700

    default database

---
+++ src/Database.js
@@ -0,0 +1,25 @@
+let defaultDB =
+{
+    "background": [
+        "mylivewallpapers.com-Yellow-Space-Suit-Girl.webm"
+    ],
+    "collection": [
+        "https://github.dev"
+    ],
+    "omniboxs": [
+        {
+            "src": "ios",
+            "des": "https://github.com/search?o=desc&q=stars%3A%3E%3D20+fork%3Atrue+language%3Aswift&s=updated&type=Repositories"
+        }
+    ],
+    "read-later": [],
+    hooks: [
+        {
+            "active": true,
+            "des": "http://localhost/assests/hello.html",
+            "src": "https://wttr.in/hanoi"
+        },
+    ],
+}
+
+export default defaultDB;
